e012e4e3b9053df183ee67f2a0831e8a4e2a7db0
Roberto Garza <robertg4567@hotmail.com>
2018-07-06 01:21:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,22 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 
-def makeboard
+def display_board
+
+  cells = Array.new
+  cells[0] = "   "
+  cells[1] = "   "
+  cells[2] = "   "
+
+  cells[3] = "   "
+  cells[4] = "   "
+  cells[5] = "   "
+
+  cells[6] = "   "
+  cells[7] = "   "
+  cells[8] = "   "
   puts "Welcome to Tic Tac Toe"
+  
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -10,4 +24,4 @@ def makeboard
   puts "   |   |   "
 end
 
-makeboard
+display_board
